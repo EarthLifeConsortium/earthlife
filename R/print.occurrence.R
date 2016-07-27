7b8f11d695e05dc073aad650944f9110a127fe86
Simon <simon.j.goring@gmail.com>
2016-07-27 16:17:17 -0700

    Another minor modification of the `print` method to deal with `bind` returned `occurrence` objects.

--- R/print.occurrence.R
+++ R/print.occurrence.R
@@ -1,22 +1,19 @@
 #' @export
 print.occurrence <- function(x, ...) {
-  citation <- paste0("EarthLife Consortium. ",
-                     format(x$meta$access, "%Y"), ". ",
-                     x$meta$title, ". ",
-                     "Access Date:",
-                     format(x$meta$access, "%F"), "; ",
-                     "URL: ",
-                     x$meta$call, '\n\n')
 
   uniques <- nrow(x$records) - sum(duplicated(x$records[,c("lng", "lat")]))
   age_range <- c(range(x$records$max_age, na.rm = TRUE)[1],
                  range(x$records$min_age, na.rm = TRUE)[2])
 
+  access_date <- ifelse("access" %in% names(x$meta),
+                        format(x$meta$access, "%F"),
+                        paste0(sapply(x$meta, function(x) format(x$access, "%F")), collapse = ", "))
+
   record <- paste0("Occurrence data from the EarthLife Consortium.\n",
                    nrow(x$records), " occurrences returned from ", uniques, " unique sites.\n",
                    length(unique(x$records$accepted_name)), " identified taxa from ",
                    age_range[1], " to ", age_range[2], " Mya.\n",
-                   "Accessed: ",format(x$meta$access, "%F"),"\n\n")
+                   "Accessed: ",access_date,"\n\n")
 
   cat(record)
   print(x$record[1:3,c("lng", "lat", "accepted_name", "min_age", "max_age", "age_unit")])
